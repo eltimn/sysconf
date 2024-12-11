24feb71041a13b70fdad8478dcdddae44ba27d39
Tim Nelson <tim@eltimn.com>
2024-12-10 20:13:24 -0600

    Disable gnupg agent on cbox

--- nix/machines/cbox/configuration.nix
+++ nix/machines/cbox/configuration.nix
@@ -100,10 +100,10 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
+  #programs.gnupg.agent = {
+  #  enable = true;
+  #  enableSSHSupport = true;
+  #};
   programs.zsh.enable = true;
 
   # List services that you want to enable:
