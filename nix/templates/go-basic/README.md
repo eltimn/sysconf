7251f6e66af7f1dea896c495b39e494d0b9106f0
Tim Nelson <tim@eltimn.com>
2025-01-10 20:31:24 -0600

    Basic go template

---
+++ nix/templates/go-basic/README.md
@@ -0,0 +1,14 @@
+# Go App
+
+## Dev Shell
+
+To use the nix dev shell you do one of two things:
+
+- If you're using Direnv, simply run `direnv allow`.
+- To manually enter the dev shell, run `nix develop`.
+
+## Setup
+
+```shell
+go mod init <module name>
+```
---
+++ nix/templates/go-basic/flake.nix
@@ -0,0 +1,82 @@
+{
+  description = "Go/Templ Application";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+    }:
+    let
+      # System types to support.
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            inherit system;
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ self.overlays.default ];
+            };
+            pkgs-unstable = import nixpkgs-unstable {
+              inherit system;
+              overlays = [ self.overlays.default ];
+            };
+          }
+        );
+
+      goPkgName = "go"; # 1.23
+      cgoEnabled = 0;
+    in
+    {
+      # overlays to use a specific version as the main package
+      overlays.default = final: prev: {
+        go = prev.${goPkgName};
+      };
+
+      devShell = forAllSystems (
+        {
+          system,
+          pkgs,
+          pkgs-unstable,
+          ...
+        }:
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+          ];
+
+          packages =
+            with pkgs;
+            [
+              go-tools
+              golangci-lint
+              gotools
+            ]
+            ++ [ pkgs-unstable.gopls ];
+
+          env = {
+            CGO_ENABLED = cgoEnabled;
+          };
+
+          shellHook = ''
+            echo "Welcome to Go App!"
+            echo "`${pkgs.go}/bin/go version`"
+          '';
+        }
+      );
+    };
+}
