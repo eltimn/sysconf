b05036c1f58cd40621290ea2bda61b23af568bfe
Tim Nelson <tim@eltimn.com>
2025-01-26 08:17:50 -0600

    Moved common files from nix -> dotfiles

---
+++ dotfiles/common/bin/common/geek-timer.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "Timer fired: $(date)"
+echo "PATH: $PATH"
+
+exit 0
--- nix/home/common/default.nix
+++ nix/home/common/default.nix
@@ -1,21 +1,6 @@
 { pkgs, ... }:
 {
   home = {
-    # List of files to be symlinked into the user home directory.
-    file.".ackrc".source = ./files/.ackrc;
-    file.".ansible.cfg".source = ./files/.ansible.cfg;
-    file.".gitignore".source = ./files/.gitignore;
-    file.".mongoshrc.js".source = ./files/.mongoshrc.js;
-
-    # file."bin".source = ./files/bin;
-    # file."bin/common".source = ./files/bin;
-
-    # links individual files
-    file."bin/common" = {
-      source = ./files/bin;
-      recursive = true;
-    };
-
     packages = with pkgs; [
       # ack-grep
       bitwarden-cli
