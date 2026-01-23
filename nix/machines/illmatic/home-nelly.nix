0a90eacbb324e6fc172982f63ef9e6a34cefd61d
Tim Nelson <tim@eltimn.com>
2026-01-22 23:28:18 -0600

    Move pictures to illmatic

--- nix/machines/illmatic/home-nelly.nix
+++ nix/machines/illmatic/home-nelly.nix
@@ -32,12 +32,12 @@
         "/mnt/backup/ruca/nelly/Audio"
         "/mnt/backup/ruca/nelly/Documents"
         "/mnt/backup/ruca/nelly/Notes"
-        "/mnt/backup/ruca/nelly/Pictures"
         "/mnt/backup/ruca/nelly/secret-cipher"
         "/mnt/backup/ruca/nelly/sysconf"
         "/mnt/backup/ruca/nelly/workspaces"
         "/mnt/backup/ruca/nelly/zen"
         "/mnt/music"
+        "/mnt/pictures"
       ];
       passwordPath = "/run/keys/borg-passphrase-illmatic";
     };
