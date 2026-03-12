ff1b3f68a915512e56eea3af26820607099a23c4
Tim Nelson <tim@eltimn.com>
2026-03-11 19:52:09 -0500

    Thunar module

---
+++ nix/modules/system/desktop/thunar.nix
@@ -0,0 +1,33 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.sysconf.desktop.thunar;
+in
+{
+  options.sysconf.desktop.thunar = {
+    enable = lib.mkEnableOption "thunar";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.thunar = {
+      enable = true;
+      plugins = with pkgs.xfce; [
+        thunar-archive-plugin
+        thunar-volman
+      ];
+    };
+
+    environment.systemPackages = with pkgs; [
+      mate.engrampa
+    ];
+
+    programs.xfconf.enable = true; # Needed to save preferences when not using XFCE desktop environment
+
+    services.gvfs.enable = true; # Mount, trash, and other functionalities
+    services.tumbler.enable = true; # Thumbnail support for images
+  };
+}
