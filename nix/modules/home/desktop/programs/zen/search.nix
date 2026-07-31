7cb54ec242448224242e97a2672cbfd249caf9f5
Tim Nelson <tim@eltimn.com>
2026-07-30 22:17:00 -0500

    Make ddg the default search engine in zen

--- nix/modules/home/desktop/programs/zen/search.nix
+++ nix/modules/home/desktop/programs/zen/search.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   force = true;
-  default = "SearXNG";
+  default = "DuckDuckGo";
   engines =
     let
       nixSnowflakeIcon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
@@ -137,7 +137,7 @@
         ];
       };
 
-      "ddg" = {
+      "DuckDuckGo" = {
         urls = [
           {
             template = "https://duckduckgo.com";
@@ -156,8 +156,6 @@
         definedAliases = [
           "duck"
           "ddg"
-          "dck"
-          "dckk"
         ];
       };
 
