5b0181321cc09143ae74e689aff0d637b8c13f5d
Tim Nelson <tim@eltimn.com>
2026-07-30 21:29:36 -0500

    Added TODO for a darkman setting

--- nix/modules/home/desktop/niri/darkman.nix
+++ nix/modules/home/desktop/niri/darkman.nix
@@ -89,6 +89,7 @@ in
           name = "Adwaita";
           package = pkgs.adwaita-icon-theme;
         };
+        # TODO: resolve this
         # The default value of `gtk.gtk4.theme` has changed from `config.gtk.theme` to `null`.
         #   You are currently using the legacy default (`config.gtk.theme`) because `home.stateVersion` is less than "26.05".
         #   To silence this warning and keep legacy behavior, set:
